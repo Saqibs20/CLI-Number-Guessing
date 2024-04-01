160b8fb32736270b179b7bdc900cb99fb3fe1872
Rana saqib <160149771+Saqibs20@users.noreply.github.com>
2024-04-01 18:37:20 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+#!  /usr/bin/env node
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+const answer = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "Please guess a number between 1-6:",
+    },
+]);
+if (answer.userGuessedNumber === randomNumber) {
+    console.log("Congrulations! You guessed right number.");
+}
+else {
+    console.log("you guessed wrong number");
+}
